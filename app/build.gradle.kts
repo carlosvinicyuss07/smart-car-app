4af3b514d39764b0f6fabe77badf1eda14e47c45
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-05-19 21:41:41 -0300

    Feature: Configura Flavors e Injecao de Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,31 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
+    }
+
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-DEV"
+
+            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/\"")
+        }
+        create("prod") {
+            dimension = "environment"
+
+            buildConfigField("String", "BASE_URL", "\"https://api-temporaria-placeholder.com/\"")
+        }
     }
 }
 
 dependencies {
 
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/main/java/com/glc/smartcar/di/AppModules.kt
@@ -0,0 +1,11 @@
+package com.glc.smartcar.di
+
+import org.koin.dsl.module
+
+val networkModule = module {
+
+}
+
+val appModule = module {
+
+}
