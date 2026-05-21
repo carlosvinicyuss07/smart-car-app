83e155f65f18655abc64156c0379940609225b3c
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-05-21 20:07:51 -0300

    Feature: Definicao de contrato e implementacao do FipeRepository

---
+++ app/src/main/java/com/glc/smartcar/data/repository/FipeRepositoryInterface.kt
@@ -0,0 +1,11 @@
+package com.glc.smartcar.data.repository
+
+import com.glc.smartcar.data.model.fipe.FipeDetailResponse
+import com.glc.smartcar.data.model.fipe.FipeItemResponse
+
+interface FipeRepositoryInterface {
+    suspend fun listarMarcas(): Result<List<FipeItemResponse>>
+    suspend fun listarModelos(brandId: String): Result<List<FipeItemResponse>>
+    suspend fun listarAnos(brandId: String, modelId: String): Result<List<FipeItemResponse>>
+    suspend fun obterPrecoFipe(brandId: String, modelId: String, yearId: String): Result<FipeDetailResponse>
+}
