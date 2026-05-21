fdcb9559095a67972ff0082778e521529f73b911
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2026-05-21 06:47:28 -0300

    Feature: Criacao de wrapper de resposta (Result.kt)

---
+++ app/src/main/java/com/glc/smartcar/data/repository/Result.kt
@@ -0,0 +1,6 @@
+package com.glc.smartcar.data.repository
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val message: String, val code: Int? = null) : Result<Nothing>()
+}
